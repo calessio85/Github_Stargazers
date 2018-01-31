c0bd5eef1d1b7a93d05592b514d46d94d7c9e357
Alessio <alessio.celli@ingeniars.com>
2018-01-31 09:22:29 +0100

    Added network communication tests

--- app/src/androidTest/java/it/celli/testgithub/NetworkCommunicationTest.java
+++ app/src/androidTest/java/it/celli/testgithub/NetworkCommunicationTest.java
@@ -4,22 +4,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
-import android.support.test.espresso.ViewAction;
-import android.support.test.espresso.ViewInteraction;
-import android.support.test.espresso.action.ViewActions;
-import android.support.test.espresso.assertion.ViewAssertions;
-import android.support.test.espresso.contrib.RecyclerViewActions;
-import android.support.test.espresso.matcher.CursorMatchers;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.v7.widget.RecyclerView;
-import android.widget.EditText;
 
 import com.squareup.okhttp.mockwebserver.MockResponse;
 import com.squareup.okhttp.mockwebserver.MockWebServer;
-
-import junit.framework.Assert;
 
 import org.hamcrest.Matchers;
 import org.junit.Before;
@@ -36,20 +25,15 @@ import it.celli.testgithub.view.MainActivity;
 
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.RootMatchers.isDialog;
-import static android.support.test.espresso.matcher.ViewMatchers.assertThat;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class NetworkCommunicationTest {
 
     private static final String TEST_OWNER = "octocat";
-    private static final String TEST_REPO = "prova";
 
     private MockWebServer mockWebServer;
-    private EditText owner, repo;
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class, true, false);
